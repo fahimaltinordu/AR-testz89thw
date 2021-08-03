7c284c7c7aac4541a83f717b8604b35499e07868
Fahim Altınordu <altinordu.fahim@gmail.com>
2021-08-03 19:34:30 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -11,25 +11,23 @@ window.onload = () => {
         //lat-long info for bottom
         document.getElementById('currentlocation').innerHTML=`latitude: ${position.coords.latitude.toFixed(6)} || longitude: ${position.coords.longitude.toFixed(6)}`;
         
-        // let radius = position.coords.accuracy;
-        // let mymap = L.map('mapid').setView([position.coords.latitude, position.coords.longitude], 16);
 
-        //leaflet.js part
+        //leaflet.js map tracking
         var mymap = L.map('mapid', {
             center: [position.coords.latitude, position.coords.longitude],
             zoom: 16
         });
         mymap.locate({setView: true, watch: true});
 
+
         var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
         .bindPopup('you are around here')
         .openPopup();
+
         var circle = L.circle([position.coords.latitude, position.coords.longitude], { 
             color: '#3388ff', 
             radius: 50.0 }).addTo(mymap);
         
-        
-
         
         function onLocationFound(e) {
             var lat = (e.latlng.lat);
@@ -39,6 +37,7 @@ window.onload = () => {
             circle.setLatLng(newLatLng); 
         }
         mymap.on('locationfound', onLocationFound);
+
         
         const titleURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
         const attribution = '© OpenStreetMap | © Mapbox';
@@ -49,7 +48,7 @@ window.onload = () => {
             maxZoom: 17, 
             minZoom: 1, 
             tileSize: 512, 
-            accessToken: 'pk.eyJ1IjoiZmFoaW1hbHRpbm9yZHUiLCJhIjoiY2tycnl0NGljMzN6ODJ2cGVzNzg5eWZ2bSJ9.iEZejnMqCLmdIcCkOJcvxg'
+            accessToken: 'pk.eyJ1IjoiZmFoaW1hbHRpbm9yZHUiLCJhIjoiY2tycnl0NGljMzN6ODJ2cGVzNzg5eWZ2bSJ9.iEZejnMqCLmdIcCkOJcvxg',
         });
         tiles.addTo(mymap);
         
@@ -73,3 +72,5 @@ window.onload = () => {
     }
 
 }
+
+//leaflet.js add coin icons to map
